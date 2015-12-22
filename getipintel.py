1921550e97bf5518ef1acaf59445a4ab3e9d9af2
black <blackice.sh@gmail.com>
2015-12-21 20:55:11 -0600

    added some python code, initial commit

---
+++ getipintel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import requests
+import sys
+def checkIP(ip):
+	#change this variable to your email address
+	contactEmail="contact@getipintel.net"
+	#if probability from getIPIntel is grater than this value, return 1
+	maxProbability=0.99
+	#if you wish to use flags or json format, edit the request below
+	result = requests.get("http://check.getipintel.net/check.php?ip="+ip+"&contact="+contactEmail)
+	if (result.status_code != 200) or (int(result.content) < 0):
+		sys.stderr.write("An error occured while querying GetIPIntel")
+	if (int(result.content) > maxProbability):
+		return 1;
+	else:
+		return 0;
+
