6953f90cf17336118706f884dc0e569b62be3691
black <blackice.sh@gmail.com>
2016-01-12 17:56:35 -0600

    added timeout value

--- getipintel.py
+++ getipintel.py
@@ -6,8 +6,9 @@ def checkIP(ip):
 	contactEmail="YourEmailAddress"
 	#if probability from getIPIntel is grater than this value, return 1
 	maxProbability=0.99
+	timeout=5.00
 	#if you wish to use flags or json format, edit the request below
-	result = requests.get("http://check.getipintel.net/check.php?ip="+ip+"&contact="+contactEmail)
+	result = requests.get("http://check.getipintel.net/check.php?ip="+ip+"&contact="+contactEmail, timeout=timeout)
 	if (result.status_code != 200) or (int(result.content) < 0):
 		sys.stderr.write("An error occured while querying GetIPIntel")
 	if (int(result.content) > maxProbability):
